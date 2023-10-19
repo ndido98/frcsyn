7879a15c7819704c4c6b4eaf04ca24b4e84d0bd8
ndido98 <ndido98@gmail.com>
2023-10-19 11:32:54 +0200

    switch to enroot

--- slurm/train_baseline_dgx.sh
+++ slurm/train_baseline_dgx.sh
@@ -4,10 +4,11 @@
 #SBATCH --partition=dgx_usr_preempt
 #SBATCH --job-name=frcsyn-train_baseline
 #SBATCH --nodes=1
+#SBATCH --nodelist=dgx02
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=256G
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:4
 #SBATCH --time=24:00:00
 #SBATCH --error=slurm_train_baseline_%A_%a.err
 #SBATCH --output=slurm_train_baseline_%A_%a.out
@@ -18,28 +19,26 @@ set -e
 
 scratch_home=$CINECA_SCRATCH/frcsyn
 project_dir=$scratch_home/frcsyn
-datasets_dir=$scratch_home/datasets
+datasets_dir=/raid/scratch_local/frcsyn/datasets
 output_dir=$scratch_home/job_$SLURM_JOB_ID
 
 cd $scratch_home
-echo "=== Pulling singularity image ==="
-if [ ! -f pytorch_23.09-py3.sif ]; then
-    singularity pull docker://nvcr.io/nvidia/pytorch:23.09-py3
-fi
-singularity exec --bind $project_dir:/mnt/frcsyn pytorch_23.09-py3.sif pip install -r /mnt/frcsyn/requirements.txt
-
-echo "=== Copying datasets to $TMPDIR ==="
-mkdir -p $TMPDIR/datasets
-cp -r $datasets_dir/* $TMPDIR/datasets/
+echo "=== Opening enroot container ==="
+export ENROOT_CACHE_PATH=/raid/scratch_local/frcsyn/tmp/enroot-cache/group-$(id -g)
+export ENROOT_DATA_PATH=/raid/scratch_local/frcsyn/tmp/enroot-data/user-$(id -u)
+export ENROOT_RUNTIME_PATH=/raid/scratch_local/frcsyn/tmp/enroot-runtime/user-$(id -u)
+export ENROOT_MOUNT_HOME=y
+export NVIDIA_DRIVER_CAPABILITIES=all
 
 echo "=== Running training ==="
 mkdir -p $TMPDIR/tmpdir
-singularity exec \
-    --bind $TMPDIR/datasets:/mnt/datasets,$project_dir:/mnt/frcsyn,$TMPDIR/tmpdir:/mnt/tmpdir \
-    --nv pytorch_23.09-py3.sif \
-    python /mnt/frcsyn/main.py fit \
-    -c /mnt/frcsyn/experiments/train.yml -c /mnt/frcsyn/experiments/real.yml \
-    --data.datasets.root /mnt/datasets --model.temp_dir /mnt/tempdir
+enroot start \
+    --mount $datasets_dir:/workspace/datasets --mount $project_dir:/workspace/frcsyn --mount $TMPDIR/tmpdir:/workspace/tmpdir \
+    --root \
+    --env NVIDIA_DRIVER_CAPABILITIES \
+    --rw \
+    pytorch2309 \
+    sh -c 'python /workspace/frcsyn/main.py fit -c /workspace/frcsyn/experiments/train.yml -c /workspace/frcsyn/experiments/real.yml --trainer.strategy ddp'
 
 echo "=== Done, copying back data ==="
 mkdir -p $output_dir
